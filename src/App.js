5c94a8c6c66a6bc2a89cb3f3b44a00bf7de48907
danielecortes89 <danielecortes.89@gmail.com>
2022-11-22 18:37:00 +0100

    Created input component and css button changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import './App.css';
 import Button from './components/Button';
-import './stylesheets/Button.css'
+import './stylesheets/Button.css';
+import Input from './components/Input'
 
 function App() {
   return (
     <div className="App">
       <div className='calculator-container'>
+        <Input />
         <div className='row'>
           <Button>
             1
           </Button>
           <Button>
+            2
+          </Button>
+          <Button>
+            3
+          </Button>
+          <Button>
             +
           </Button>
         </div>
-        <div className='row'></div>
-        <div className='row'></div>
-        <div className='row'></div>
+        <div className='row'>
+          <Button>
+            4
+          </Button>
+          <Button>
+            5
+          </Button>
+          <Button>
+            6
+          </Button>
+          <Button>
+            -
+          </Button>
+        </div>
+        <div className='row'>
+          <Button>
+            7
+          </Button>
+          <Button>
+            8
+          </Button>
+          <Button>
+            9
+          </Button>
+          <Button>
+            *
+          </Button>
+        </div>
+        <div className='row'>
+          <Button>
+            =
+          </Button>
+          <Button>
+            0
+          </Button>
+          <Button>
+            .
+          </Button>
+          <Button>
+            /
+          </Button>
+          
+        </div>
         <div className='row'></div>
       </div>
     </div>
